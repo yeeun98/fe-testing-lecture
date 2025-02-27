30e407821a89cf3f5a1fe2164995a7112d99ff1d
yeeun_kim_ <dfeb23rd@gmail.com>
2025-02-27 12:56:47 +0900

    2-1.2:테스트 주도 개발과 프런트엔드 테스트 예제

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -2,14 +2,6 @@ import cn from 'classnames';
 import React, { useState } from 'react';
 import './text.css';
 
-/**
- * placeholder 설정 - 테스트 작성 완료
- * className에 따른 css class 설정 - 테스트 작성 완료
- * 텍스트를 입력할 때마다 onChange 핸들러 호출
- * focus 시 border 스타일 변경
- * focus 시 onFocus 핸들러 호출
- * Enter 키 입력 시 onEnter 핸들러 호출
- */
 export default function TextField({
   placeholder,
   className,
--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -7,3 +7,22 @@ import render from '@/utils/test/render';
 it('sample test.', async () => {
   expect(1).toBe(1);
 });
+
+it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+  await render(<TextField />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  expect(textInput).toBeInTheDocument();
+});
+
+it('텍스트를 입력하면 onChange prop으로 등록한 함수가 호출된다.', async () => {
+  const spy = vi.fn();
+  
+  const { user } = await render(<TextField onChange={spy} />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  await user.type(textInput, 'test');
+  expect(textInput).toHaveBeenCalledWith('test');
+});
