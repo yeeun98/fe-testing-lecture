9fe68e8a6e005d4fc738c0bb257b8aabb70c241f
김예은 <dfeb23rd@gmail.com>
2025-03-01 16:46:04 +0900

    2.1 UI 테스트와 스냅샷 테스트

--- src/pages/cart/components/tests/PageTitle.spec.jsx
+++ src/pages/cart/components/tests/PageTitle.spec.jsx
@@ -3,6 +3,16 @@ import React from 'react';
 import PageTitle from '@/pages/cart/components/PageTitle';
 import render from '@/utils/test/render';
 
-it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {});
+// 테스트 파일 내에 스냄샷까지 함께 관리하고 싶은 경우
+it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
 
-it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {});
+  expect(container).toMatchInlineSnapshot();
+});
+
+// 테스트 파일과 스냅샷 파일을 별도로 관리하고 싶은 경우
+it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
+
+  expect.toMatchSnapshot();
+});
